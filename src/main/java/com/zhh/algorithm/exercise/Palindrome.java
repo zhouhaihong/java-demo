3c9bc854736c0433f3c4e96bf9ba47e9af7dd201
zhh <zhouhaihong@cechealth.cn>
2019-06-24 11:36:53 +0800

    added palindrome exercise.

---
+++ src/main/java/com/zhh/algorithm/exercise/Palindrome.java
@@ -0,0 +1,32 @@
+/*********************************************************************************
+ * Copyright (c) 2019 中电健康云科技有限公司
+ * 版本      DATE                 BY             REMARKS
+ * ----  -----------  ---------------  ------------------------------------------
+ * 1.0    2019/6/19      zhh           init.
+ ********************************************************************************/
+package com.zhh.algorithm.exercise;
+
+/**
+ * 判断给定字符串是否是回文字符串
+ * @Description:
+ * @Author: zhh
+ * @Date: 2019/6/19
+ */
+public class Palindrome {
+
+    public static boolean isPalindrome(String str){
+        char[] chars = str.toCharArray();
+        for (int i = 0, j = chars.length-1; i < chars.length; i++,j--) {
+            if(chars[i] != chars[j]){
+                return false;
+            }else if(i<=j){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Palindrome.isPalindrome("hhdfhh"));
+    }
+}
