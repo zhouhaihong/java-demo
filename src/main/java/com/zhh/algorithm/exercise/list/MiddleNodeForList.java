8fb04a0c8bf33a377f2870686cce40cc36196084
zhouhaihong <zhhandzqx@163.com>
2021-01-26 14:02:42 +0800

    add stack.

--- src/main/java/com/zhh/algorithm/exercise/list/MiddleNodeForList.java
+++ src/main/java/com/zhh/algorithm/exercise/list/MiddleNodeForList.java
@@ -6,26 +6,25 @@ package com.zhh.algorithm.exercise.list;
 public class MiddleNodeForList {
     /**
      * 快慢指针法
+     * 1.快指针结束条件
+     * 2.链表元素为奇偶的情况
      * @param head
      * @return
      */
     public ListNode middleNode(ListNode head){
-        if(head == null || head.next == null){
-            return head;
-        }
+         if(head == null || head.next == null){
+             return head;
+         }
+         ListNode slow = head;
+         ListNode fast = head.next;
+         while (fast!=null && fast.next!=null){
+             slow = slow.next;
+             fast = fast.next.next;
+         }
 
-        ListNode slow = head;
-        ListNode fast = head.next;
-
-        while (fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        if(fast !=null){
-            slow = slow.next;
-        }
-
-        return slow;
+         if(fast!=null){
+             slow = slow.next;
+         }
+         return slow;
     }
 }
--- src/main/java/com/zhh/algorithm/exercise/list/RemoveNthFromEnd.java
+++ src/main/java/com/zhh/algorithm/exercise/list/RemoveNthFromEnd.java
@@ -1,12 +1,64 @@
 package com.zhh.algorithm.exercise.list;
 
+import java.util.List;
+
 /**
  * Leedcode 19. 删除链表的倒数第 N 个结点
  */
 public class RemoveNthFromEnd {
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+    /**
+     * 计算链表长度遍历法
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+       ListNode dump = new ListNode();
+       dump.next = head;
+       int i = 1;
+       int len = getLength(head);
+       ListNode temp = dump;
+       while (i < (len - n + 1)){
+           temp = temp.next;
+       }
+       temp.next = temp.next.next;
+       return dump.next;
+    }
+
+    private int getLength(ListNode head){
+        int len = 0;
+        while (head != null){
+            head = head.next;
+        }
+        return len;
+    }
+
+    /**
+     * 栈
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
 
         return null;
     }
+
+    /**
+     * 一次遍历
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd3(ListNode head, int n) {
+
+        return null;
+    }
+
+
+
+
+
+
 }
